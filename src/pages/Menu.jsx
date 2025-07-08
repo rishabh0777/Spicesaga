df47bb30d67538c1d80ef93d1b7f4b40cfbe29ee
Rishabh Srivastava <104848594+rishabh0777@users.noreply.github.com>
2025-07-08 07:39:27 +0000

    Refactor About and Menu pages; enhance layout and styling for improved user experience

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -6,48 +6,60 @@ import Tag from '../components/Tag';
 
 const Menu = () => {
   return (
-    <div className='w-full min-h-screen relative bg-red-900 text-white flex flex-col justify-between overflow-hidden'>
+    <div className='w-full relative bg-red-900 text-white pt-[10vh]'>
       
       {/* Heading */}
-      <h1 className='text-[6vw] text-center pt-8'>ON THE MENU</h1>
+      <h1 className='text-[9vw] md:text-[6vw] text-center mb-4 md:mb-0'>ON THE MENU</h1>
 
-      {/* Bottom Image Section */}
-      <div className="w-full h-[60vh] flex">
-        {/* First image */}
-        <div className='w-[33.3%] relative h-full overflow-hidden'>
-          <img className='w-full h-full object-cover absolute top-0 left-0' src={Gournet} alt="Gourmet" />
+      {/* Image Section */}
+      <div className="w-full flex flex-col md:flex-row">
+        {/* Image 1 */}
+        <div className='w-full md:w-1/3 h-[40vh] md:h-[60vh] relative overflow-hidden'>
+          <img
+            className='w-full h-full object-cover absolute top-0 left-0'
+            src={Gournet}
+            alt="Gourmet"
+          />
           <div className='absolute z-10 h-full w-full flex justify-center items-center'>
             <Tag
-            text="gaurmet plates"
-              className="text-[1.5vw] font-bold text-white border-white"
+              text="gourmet plates"
+              className="sm:w-[60vw] sm:h-[30vw] md:w-[18vw] md:h-[9vw] text-[5vw] md:text-[1.3vw] font-bold text-white border-white"
             />
           </div>
         </div>
 
-        {/* Second image */}
-        <div className='w-[33.3%] relative h-full overflow-hidden'>
-          <img className='w-full h-full object-cover absolute top-0 left-0' src={Brews} alt="Brews" />
+        {/* Image 2 */}
+        <div className='w-full md:w-1/3 h-[40vh] md:h-[60vh] relative overflow-hidden'>
+          <img
+            className='w-full h-full object-cover absolute top-0 left-0'
+            src={Brews}
+            alt="Brews"
+          />
           <div className='absolute z-10 h-full w-full flex justify-center items-center'>
             <Tag
               text="craft brews"
-              className="text-[1.5vw] font-bold text-white border-white"
+              className="sm:w-[60vw] sm:h-[30vw] md:w-[18vw] md:h-[9vw] text-[5vw] md:text-[1.3vw] font-bold text-white border-white"
             />
           </div>
         </div>
 
-        {/* Third image */}
-        <div className='w-[33.3%] relative h-full overflow-hidden'>
-          <img className='w-full h-full object-cover absolute top-0 left-0' src={DishVine} alt="Dish Vine" />
+        {/* Image 3 */}
+        <div className='w-full md:w-1/3 h-[40vh] md:h-[60vh] relative overflow-hidden'>
+          <img
+            className='w-full h-full object-cover absolute top-0 left-0'
+            src={DishVine}
+            alt="Wine & Dish"
+          />
           <div className='absolute z-10 h-full w-full flex justify-center items-center'>
             <Tag
               text="wine & dish pairings"
-              className="text-[1.5vw] font-bold text-white border-white"
+              className="sm:w-[60vw] sm:h-[30vw] md:w-[18vw] md:h-[9vw] text-[5vw] md:text-[1.3vw] font-bold text-white border-white"
             />
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Menu;
