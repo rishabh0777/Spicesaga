0b74268002a2c9f3cbd1c66c5590da0622097d4a
Rishabh Srivastava <104848594+rishabh0777@users.noreply.github.com>
2025-07-08 07:06:05 +0000

    Refactor Hero component; enhance layout for improved responsiveness and styling

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -3,7 +3,7 @@ import Tag from "../components/Tag"
 
 const Hero = () => {
   return (
-    <div className='w-full h-[120vh] relative'>
+    <div className='w-full sm:h-[40vh] md:h-[120vh] relative'>
       <div className='absolute inset-0 z-0'>
         <img 
           className='w-full h-full object-cover object-center'
@@ -12,13 +12,14 @@ const Hero = () => {
         />
       </div>
 
-      <div className='relative z-10 h-full top-[15%] left-[10%]'>
-        <h1 className='text-4xl font-bold text-orange text-red-900 text-[9.5vw]'>Spice<br />Saga</h1>
+      <div className='relative z-10 h-full top-[15%] left-[5%] md:left-[10%]'>
+        <h1 className='md:text-[9vw] leading-none font-bold text-orange text-red-900 sm:text-[9.5vw]'>Spice<br />Saga</h1>
       </div>
 
-      <div className='relative bottom-[30%] left-[10%] left-0 z-10'>
+      <div className='relative bottom-[30%] sm:left-[5%] md:left-[10%] left-0 z-10'>
         <Tag
         text="Where tradition meets taste"
+        className="w-[30vw] h-[15vw] md:w-[24vw] md:h-[12vw] sm:text-[2vw] md:text-[1.5vw] text-center"
         />
       </div>
     </div>
